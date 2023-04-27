146060a8ededdd525804acad668a528c475ba0d6
Hunter Jennings <38039731+h-jennings@users.noreply.github.com>
2023-04-27 14:54:16 -0400

    updating vscode lua config

--- .config/vscode_neovim/init.lua
+++ .config/vscode_neovim/init.lua
@@ -13,7 +13,6 @@ vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.smartcase = true -- smart case
 vim.opt.updatetime = 300 -- faster completion (4000ms default)
 vim.opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
-
 local opts = { noremap = true, silent = true }
 
 -- Shorten function name
@@ -35,11 +34,7 @@ vim.g.maplocalleader = " "
 -- Normal --
 keymap("n", "J", "5j", opts)
 keymap("n", "K", "5k", opts)
-keymap("n", "<leader>/", ":nohlsearch<CR>", opts)
-
--- Better tabbing
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", "<gv", opts)
+keymap("n", "<leader>/", ":nohlsearch<CR>", opts) -- clear highlights
 
 -- File actions
 keymap("n", "<leader>w", "<Cmd>call VSCodeNotify('workbench.action.files.save')<CR>", opts)
@@ -52,17 +47,11 @@ keymap("n", "gr", "<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", 
 keymap("n", "gy", "<Cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>", opts)
 keymap("n", "gi", "<Cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>", opts)
 keymap("n", "gH", "<Cmd>call VSCodeNotify('editor.action.showDefinitionPreviewHover')<CR>", opts)
-keymap("n", "<leader>ca", "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>", opts)
-keymap("n", "[d", "<Cmd>call VSCodeNotify('editor.action.marker.next')<CR>", opts)
-keymap("n", "]d", "<Cmd>call VSCodeNotify('editor.action.marker.prev')<CR>", opts)
-keymap("n", "<leader>rn", "<Cmd>call VSCodeNotify('editor.action.rename')<CR>", opts)
+keymap("n", "<leader>ca", "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>", opts) -- code action quick fix
+keymap("n", "[d", "<Cmd>call VSCodeNotify('editor.action.marker.next')<CR>", opts) -- next diagnostic
+keymap("n", "]d", "<Cmd>call VSCodeNotify('editor.action.marker.prev')<CR>", opts) -- previous diagnostic
+keymap("n", "<leader>rn", "<Cmd>call VSCodeNotify('editor.action.rename')<CR>", opts) -- rename symbol
 keymap("n", "<leader>fw", "<Cmd>call VSCodeNotify('actions.find')<CR>", opts)
-keymap(
-	"n",
-	"<leader>pfw",
-	"<Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': expand('<cword>')})<CR>",
-	opts
-)
 
 -- File Navigation
 keymap("n", "<leader>ff", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", opts)
@@ -70,11 +59,12 @@ keymap("n", "<leader>P", "<Cmd>call VSCodeNotify('workbench.action.showCommands'
 keymap(
 	"n",
 	"<leader>o",
-	"<Cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup')<CR>",
+	"<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>",
 	opts
 )
 keymap("n", "<leader>O", "<Cmd>call VSCodeNotify('workbench.action.closeEditorsInOtherGroups')<CR>", opts)
 keymap("n", "<leader>F", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", opts)
+keymap("n", "<leader>pfw", "<Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': expand('<cword>')})<CR>", opts)
 
 -- UI
 keymap("n", "-", "<Cmd>call VSCodeNotify('revealInExplorer')<CR>", opts)
@@ -84,6 +74,7 @@ keymap("n", "<leader>`", "<Cmd>call VSCodeNotify('workbench.action.terminal.togg
 keymap("n", "<leader>cl", "<Cmd>call VSCodeNotify('workbench.action.toggleCenteredLayout')<CR>", opts)
 keymap("n", "<leader>tz", "<Cmd>call VSCodeNotify('workbench.action.toggleZenMode')<CR>", opts)
 
+
 -- Debugger
 keymap("n", "<leader>ds", "<Cmd>call VSCodeNotify('workbench.action.debug.selectandstart')<CR>", opts)
 keymap("n", "<leader>dr", "<Cmd>call VSCodeNotify('workbench.action.debug.restart')<CR>", opts)
@@ -91,9 +82,13 @@ keymap("n", "<leader>df", "<Cmd>call VSCodeNotify('workbench.debug.action.focusR
 keymap("n", "<leader>D", "<Cmd>call VSCodeNotify('workbench.action.debug.disconnect')<CR>", opts)
 keymap("n", "<leader>tb", "<Cmd>call VSCodeNotify('editor.debug.action.toggleBreakpoint')<CR>", opts)
 keymap("n", "<leader>tl", "<Cmd>call VSCodeNotify('editor.debug.action.addLogPoint')<CR>", opts)
+keymap("n", "gH", "<Cmd>call VSCodeNotify('editor.debug.action.showDebugHover')<CR>", opts)
 
 -- Commenting
 keymap("n", "gc", "<Plug>VSCodeCommentary", opts)
 keymap("x", "gc", "<Plug>VSCodeCommentary", opts)
 keymap("o", "gc", "<Plug>VSCodeCommentary", opts)
 keymap("n", "gcc", "<Plug>VSCodeCommentaryLine", opts)
+
+-- Copilot
+keymap("n", "<leader>cp", "<Cmd>call VSCodeNotify('github.copilot.generate')<CR>", opts)
